1b75d7e8b61171d98d42e1b29d160dadb74a260a
Shumin Li <shuminli@eoas.ubc.ca>
2024-05-15 15:59:36 -0700

    update functions to read .csv files (sl_readflightrecord.m and sl_readphotometa.m)

---
+++ sl_matlab/test_read_csv/test_csv_readers.m
@@ -0,0 +1,27 @@
+
+
+%% Step 1, addpath of g_rect toolbox
+base_dir = '/Users/shuminli/g_rect_toolbox/'; % change to your local dir
+addpath(genpath(base_dir));
+
+
+%% test sl_readflightrecord
+fr_dir = [base_dir,'sl_matlab/test_read_csv/flight_record/'];
+fr_info = dir([fr_dir,'*.csv']);
+
+cd(fr_dir);
+
+fr_names = {fr_info.name};
+
+DJI = sl_readflightrecord(fr_names);
+
+%% test sl_photo
+
+meta_dir = [base_dir,'sl_matlab/test_read_csv/photo_meta/'];
+meta_info = dir([meta_dir,'*.csv']);
+
+cd(meta_dir);
+
+meta_names = {meta_info.name};
+
+PHOT = sl_readphotometa(meta_names);
